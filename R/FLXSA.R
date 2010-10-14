a14468bec30366bc79c2f7a12e1d9e8eeb686d41
Ernesto Jardim <ernesto.jardim@gmail.com>
2010-10-14 10:40:48 +0000

    Fixed diagnostics year range.

--- R/FLXSA.R
+++ R/FLXSA.R
@@ -570,7 +570,10 @@ print(1)
 
     cat("Regression weights\n")
     ### Calculation of time series weighting
-    yr.range <- max(dims(object@harvest)$minyear,(dims(object@harvest)$maxyear-9)):dims(object@harvest)$maxyear
+    mxy <- dims(object@harvest)$maxyear
+    mny <- dims(object@harvest)$minyear
+    if(mxy-mny>9) columns <- 9 else columns <- mxy-mny
+    yr.range <- (mxy - columns):mxy
     regWt <- FLQuant(dimnames=list(age = 'all', year = yr.range))
     for(y in yr.range) regWt[,as.character(y)] <- (1-((max(yr.range)-y)/control@tsrange)^control@tspower)^control@tspower
     print(matrix(round(regWt,3),dims(regWt)$age,dimnames=list(age="all",year=yr.range)))
